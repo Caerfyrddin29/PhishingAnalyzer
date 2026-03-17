e28652dfa55d9547ebd8355675787657be5c8e7d
Merl1 <myrddin.bellion29@gmail.com>
2026-03-17 23:55:54 +0100

    Delete PhishingAnalyzer-main directory

--- PhishingAnalyzer-main/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting PhishingAnalyzer API Server...
-echo Server will be available at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python email_api_server.py
-pause
